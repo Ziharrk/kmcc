f348f473ec8ef9e5049f585cab200f6992d8b053
Jonas Böhm <stu211735@mail.uni-kiel.de>
2023-07-18 10:03:52 +0200

    Finish external implementation of Network.Socket

--- external_files/Socket.kmcc.hs
+++ external_files/Socket.kmcc.hs
@@ -3,7 +3,10 @@
 {-# LANGUAGE BangPatterns #-}
 
 import qualified Prelude as P
+import Prelude ((*))
 import qualified Network.Socket as N
+import qualified System.IO as S
+import qualified System.Timeout as S
 import BasicDefinitions
 
 type instance HsEquivalent N.Socket = N.Socket
@@ -38,33 +41,48 @@ type Socket_Det# = N.Socket
 type Socket_ND# = N.Socket
 
 
-createSocket :: P.Integer -> P.IO N.Socket
-createSocket port = do
+listenOn :: P.Integer -> P.IO N.Socket
+listenOn port = do
   let hints = N.defaultHints{N.addrFlags = [N.AI_PASSIVE], N.addrFamily = N.AF_INET}
-  (sockInfo:_) <- N.getAddrInfo (P.Just hints) P.Nothing (P.Just (P.show (toForeign port)))
-  sock <- N.openSocket sockInfo
+  (addr:_) <- N.getAddrInfo (P.Just hints) P.Nothing (P.Just (P.show (toForeign port)))
+  sock <- N.openSocket addr
   N.setSocketOption sock N.ReuseAddr 1
-  N.bind sock (N.addrAddress sockInfo)
+  N.bind sock (N.addrAddress addr)
   N.listen sock N.maxListenQueue
   P.return sock
   
-socketdotprimuscorelistenOn_Det# = createSocket
-socketdotprimuscorelistenOn_ND# = P.error ""
+socketdotprimuscorelistenOn_Det# = listenOn
+socketdotprimuscorelistenOn_ND# = liftConvertIO1 socketdotprimuscorelistenOn_Det#
 
 socketdotlistenOnFresh_Det# = do
-  sock <- createSocket 0
+  sock <- listenOn 0
   port <- N.socketPort sock
   P.return P.$ CTuple2_Det (P.toInteger port) sock
-socketdotlistenOnFresh_ND# = P.error ""
+socketdotlistenOnFresh_ND# = P.return (P.fmap from socketdotlistenOnFresh_Det#)
 
-socketdotprimuscoresocketAccept_Det# = P.error "No implementation of socketAccept_Det"
-socketdotprimuscoresocketAccept_ND# = P.error "No implementation of socketAccept_ND"
+socketdotprimuscoresocketAccept_Det# sock = do
+  (conn, addr) <- N.accept sock
+  handle <- N.socketToHandle conn S.ReadWriteMode
+  P.return P.$ CTuple2_Det (fromForeign (P.show addr)) handle
+socketdotprimuscoresocketAccept_ND# = liftConvertIO1 socketdotprimuscoresocketAccept_Det#
 
-socketdotprimuscorewaitForSocketAccept_Det# = P.error "No implementation of waitForSocketAccept_Det"
-socketdotprimuscorewaitForSocketAccept_ND# = P.error "No implementation of waitForSocketAccept_Det"
+socketdotprimuscorewaitForSocketAccept_Det# sock timeout = do
+  let ioAction = socketdotprimuscoresocketAccept_Det# sock
+  let timer = (P.fromInteger timeout) * 1000
+  res <- S.timeout timer ioAction
+  case res of
+    P.Nothing -> P.return Nothing_Det
+    P.Just x -> P.return (Just_Det x)
+socketdotprimuscorewaitForSocketAccept_ND# = liftConvertIO2 socketdotprimuscorewaitForSocketAccept_Det#
 
-socketdotprimuscoresClose_Det# = P.error "No implementation of sClose_Det"
-socketdotprimuscoresClose_ND# = P.error "No implementation of sClose_ND"
+socketdotprimuscoresClose_Det# sock = fromForeign (N.close sock)
+socketdotprimuscoresClose_ND# = liftConvertIO1 socketdotprimuscoresClose_Det#
 
-socketdotprimuscoreconnectToSocket_Det# = P.error "No implementation of connectToSocket_Det"
-socketdotprimuscoreconnectToSocket_ND# = P.error "No implementation of connectToSocket_ND"
+socketdotprimuscoreconnectToSocket_Det# host port = do
+  let hints = N.defaultHints {N.addrFamily = N.AF_INET}
+  addr:_ <- N.getAddrInfo P.Nothing (P.Just (toForeign host)) (P.Just (P.show port))
+  sock <- N.openSocket addr
+  N.connect sock (N.addrAddress addr)
+  handle <- N.socketToHandle sock S.ReadWriteMode
+  P.return handle
+socketdotprimuscoreconnectToSocket_ND# = liftConvertIO2 socketdotprimuscoreconnectToSocket_Det#
