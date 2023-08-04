c2338f5ece380be42648087c8a387586a37f5ac2
Jonas Böhm <stu211735@mail.uni-kiel.de>
2023-08-04 08:46:48 +0200

    Add ND implementation of getCPUTime

--- external_files/CPUTime.kmcc.hs
+++ external_files/CPUTime.kmcc.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE MagicHash #-}
 import qualified Prelude as P
-import qualified System.CPUTime as P
+import qualified System.CPUTime as S
 import BasicDefinitions
 
-cPUTimedotgetCPUTime_Det# = fromForeign (P.getCPUTime P.>>= (P.return P.. (`P.div` (10 P.^ 9))))
-cPUTimedotgetCPUTime_ND# = P.error "No implementation of getCPUTime_ND"
+cPUTimedotgetCPUTime_Det# = fromForeign (S.getCPUTime P.>>= (P.return P.. (`P.div` (10 P.^ 9))))
+cPUTimedotgetCPUTime_ND# = P.return cPUTimedotgetCPUTime_Det#
 
 cPUTimedotgetElapsedTime_Det# = P.error "No implementation of getElapsedTime_Det"
 cPUTimedotgetElapsedTime_ND# = P.error "No implementation of getElapsedTime_ND"
