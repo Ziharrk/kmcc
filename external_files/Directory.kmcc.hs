cdee4117d5c4125352281311b69e5313b56de2f9
Jonas Böhm <stu211735@mail.uni-kiel.de>
2023-08-07 09:32:33 +0200

    Add external implementation of System.Directory

--- external_files/Directory.kmcc.hs
+++ external_files/Directory.kmcc.hs
@@ -1,39 +1,46 @@
 {-# LANGUAGE MagicHash #-}
 
 import qualified Prelude as P
+import qualified Data.Time.Clock.POSIX as D
+import qualified System.Directory as S
+import Data.Curry_Time
+import BasicDefinitions
 
-directorydotprimuscoredoesFileExist_Det# = P.error "No implementation of doesFileExist_Det"
-directorydotprimuscoredoesFileExist_ND# = P.error "No implementation of doesFileExist_ND"
+directorydotprimuscoredoesFileExist_Det# = liftForeign1 S.doesFileExist
+directorydotprimuscoredoesFileExist_ND# = liftConvertIO1 directorydotprimuscoredoesFileExist_Det#
 
-directorydotprimuscoredoesDirectoryExist_Det# = P.error "No implementation of doesDirectoryExist_Det"
-directorydotprimuscoredoesDirectoryExist_ND# = P.error "No implementation of doesDirectoryExist_ND"
+directorydotprimuscoredoesDirectoryExist_Det# = liftForeign1 S.doesDirectoryExist
+directorydotprimuscoredoesDirectoryExist_ND# = liftConvertIO1 directorydotprimuscoredoesDirectoryExist_Det#
 
-directorydotprimuscorefileSize_Det# = P.error "No implementation of fileSize_Det"
-directorydotprimuscorefileSize_ND# = P.error "No implementation of fileSize_ND"
+directorydotprimuscorefileSize_Det# = liftForeign1 S.getFileSize
+directorydotprimuscorefileSize_ND# = liftConvertIO1 directorydotprimuscorefileSize_Det#
 
-directorydotprimuscoregetModificationTime_Det# = P.error "No implementation of getModificationTime_Det"
-directorydotprimuscoregetModificationTime_ND# = P.error "No implementation of getModificationTime_ND"
+directorydotprimuscoregetModificationTime_Det# x = do
+  modTime <- S.getModificationTime (toForeign x)
+  let posixTime = D.utcTimeToPOSIXSeconds modTime
+  P.return (fromForeign posixTime)
+directorydotprimuscoregetModificationTime_ND# = liftConvertIO1 directorydotprimuscoregetModificationTime_Det#
 
-directorydotgetCurrentDirectory_Det# = P.error "No implementation of getCurrentDirectory_Det"
-directorydotgetCurrentDirectory_ND# = P.error "No implementation of getCurrentDirectory_ND"
+directorydotgetCurrentDirectory_Det# = fromForeign S.getCurrentDirectory
+directorydotgetCurrentDirectory_ND# = P.return (P.fmap from directorydotgetCurrentDirectory_Det#)
 
-directorydotprimuscoresetCurrentDirectory_Det# = P.error "No implementation of setCurrentDirectory_Det"
-directorydotprimuscoresetCurrentDirectory_ND# = P.error "No implementation of setCurrentDirectory_ND"
+directorydotprimuscoresetCurrentDirectory_Det# = liftForeign1 S.setCurrentDirectory
+directorydotprimuscoresetCurrentDirectory_ND# = liftConvertIO1 directorydotprimuscoresetCurrentDirectory_Det#
 
-directorydotprimuscoregetDirectoryContents_Det# = P.error "No implementation of getDirectoryContents_Det"
-directorydotprimuscoregetDirectoryContents_ND# = P.error "No implementation of getDirectoryContents_ND"
+directorydotprimuscoregetDirectoryContents_Det# = liftForeign1 S.getDirectoryContents
+directorydotprimuscoregetDirectoryContents_ND# = liftConvertIO1 directorydotprimuscoregetDirectoryContents_Det#
 
-directorydotprimuscorecreateDirectory_Det# = P.error "No implementation of createDirectory_Det"
-directorydotprimuscorecreateDirectory_ND# = P.error "No implementation of createDirectory_ND"
+directorydotprimuscorecreateDirectory_Det# = liftForeign1 S.createDirectory
+directorydotprimuscorecreateDirectory_ND# = liftConvertIO1 directorydotprimuscorecreateDirectory_Det#
 
-directorydotprimuscoreremoveDirectory_Det# = P.error "No implementation of removeDirectory_Det"
-directorydotprimuscoreremoveDirectory_ND# = P.error "No implementation of removeDirectory_ND"
+directorydotprimuscoreremoveDirectory_Det# = liftForeign1 S.removeDirectory
+directorydotprimuscoreremoveDirectory_ND# = liftConvertIO1 directorydotprimuscoreremoveDirectory_Det#
 
-directorydotprimuscorerenameDirectory_Det# = P.error "No implementation of renameDirectory_Det"
-directorydotprimuscorerenameDirectory_ND# = P.error "No implementation of renameDirectory_ND"
+directorydotprimuscorerenameDirectory_Det# = liftForeign2 S.renameDirectory
+directorydotprimuscorerenameDirectory_ND# = liftConvertIO2 directorydotprimuscorerenameDirectory_Det#
 
-directorydotprimuscoreremoveFile_Det# = P.error "No implementation of removeFile_Det"
-directorydotprimuscoreremoveFile_ND# = P.error "No implementation of removeFile_ND"
+directorydotprimuscoreremoveFile_Det# = liftForeign1 S.removeFile
+directorydotprimuscoreremoveFile_ND# = liftConvertIO1 directorydotprimuscoreremoveFile_Det#
 
-directorydotprimuscorerenameFile_Det# = P.error "No implementation of renameFile_Det"
-directorydotprimuscorerenameFile_ND# = P.error "No implementation of renameFile_ND"
+directorydotprimuscorerenameFile_Det# = liftForeign2 S.renameFile
+directorydotprimuscorerenameFile_ND# = liftConvertIO2 directorydotprimuscorerenameFile_Det#
