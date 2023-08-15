b5ce280624c346dff96d57c0451420d24254a3fe
Jonas Böhm <stu211735@mail.uni-kiel.de>
2023-08-15 09:56:31 +0200

    Add implementation of IOExts.execCmd

--- external_files/IOExts.kmcc.hs
+++ external_files/IOExts.kmcc.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE MagicHash #-}
 
 import qualified Prelude as P
+import qualified System.Process as S
+import BasicDefinitions
 
-iOExtsdotprimuscoreexecCmd_Det# = P.error "No implementation of execCmd_Det"
-iOExtsdotprimuscoreexecCmd_ND# = P.error "No implementation of execCmd_ND"
+iOExtsdotprimuscoreexecCmd_Det# s = do
+  (P.Just stdin, P.Just stdout, P.Just stderr, _) <- S.createProcess (S.shell (toForeign s))
+  P.return P.$ CTuple3_Det stdin stdout stderr
+iOExtsdotprimuscoreexecCmd_ND# = liftConvertIO1 iOExtsdotprimuscoreexecCmd_Det#
 
-iOExtsdotprimuscoreconnectToCmd_Det# = P.error "No implementation of connectToCmd_Det"
+iOExtsdotprimuscoreconnectToCmd_Det# s = P.error "No implementation of connectToCmd_Det"
 iOExtsdotprimuscoreconnectToCmd_ND# = P.error "No implementation of connectToCmd_ND"
