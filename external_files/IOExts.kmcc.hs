847bfb3496337702ff79e8349bee03c0776dc98f
Jonas Böhm <stu211735@mail.uni-kiel.de>
2023-08-31 12:24:39 +0200

    Fix pattern match failure in execCmd by creating new pipes when starting new shell process

--- external_files/IOExts.kmcc.hs
+++ external_files/IOExts.kmcc.hs
@@ -5,7 +5,9 @@ import qualified System.Process as S
 import BasicDefinitions
 
 iOExtsdotprimuscoreexecCmd_Det# s = do
-  (P.Just stdin, P.Just stdout, P.Just stderr, _) <- S.createProcess (S.shell (toForeign s))
+  (P.Just stdin, P.Just stdout, P.Just stderr, _) <- S.createProcess (S.shell (toForeign s)){S.std_in = S.CreatePipe,
+                                                                                             S.std_out = S.CreatePipe,
+                                                                                             S.std_err = S.CreatePipe}
   P.return P.$ CTuple3_Det stdin stdout stderr
 iOExtsdotprimuscoreexecCmd_ND# = liftConvertIO1 iOExtsdotprimuscoreexecCmd_Det#
 
