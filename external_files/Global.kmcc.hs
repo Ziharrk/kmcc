018bc9d2c3036448df06064b4ff8a83d0b7ca53b
Jonas Böhm <stu211735@mail.uni-kiel.de>
2023-09-13 09:59:21 +0200

    Add implementation of deterministic external functions in Data.Global

--- external_files/Global.kmcc.hs
+++ external_files/Global.kmcc.hs
@@ -1,12 +1,30 @@
 {-# LANGUAGE MagicHash #-}
 
 import qualified Prelude as P
+import qualified Data.IORef as D
+import qualified Data.Map as D
+import qualified System.IO.Unsafe as S
+import BasicDefinitions
 
-globaldotprimuscoreglobalT_Det# = P.error "No implementation of globalT_Det"
+type GlobalStore = D.Map P.String Untyped
+
+globals :: D.IORef GlobalStore
+globals = S.unsafePerformIO P.$ D.newIORef (D.empty)
+
+globaldotprimuscoreglobalT_Det# s v = S.unsafePerformIO P.$ do
+  store <- D.readIORef globals
+  D.writeIORef globals (D.insert (toForeign s) (untyped v) store)
+  P.return (GlobalT_Det s)
 globaldotprimuscoreglobalT_ND# = P.error "No implementation of globalT_ND"
 
-globaldotprimuscorereadGlobalT_Det# = P.error "No implementation of readGlobalT_Det"
+globaldotprimuscorereadGlobalT_Det# (GlobalT_Det s) = do
+  store <- D.readIORef globals
+  case D.lookup (toForeign s) store of
+    P.Nothing -> P.error ("Invalid global: " P.++ (toForeign s))
+    P.Just v  -> P.return (typed v)
 globaldotprimuscorereadGlobalT_ND# = P.error "No implementation of readGlobalT_ND"
 
-globaldotprimuscorewriteGlobalT_Det# = P.error "No implementation of writeGlobalT_Det"
+globaldotprimuscorewriteGlobalT_Det# (GlobalT_Det s) v = fromForeign P.$ do
+  store <- D.readIORef globals
+  D.writeIORef globals (D.insert (toForeign s) (untyped v) store)
 globaldotprimuscorewriteGlobalT_ND# = P.error "No implementation of writeGlobalT_ND"
