4b105462ac59a9b681f057ce807fc86539f2583d
Kai Prott <kai.prott@hotmail.de>
2025-03-19 09:21:01 +0100

    Update external File for Global

--- external_files/Global.kmcc.hs
+++ external_files/Global.kmcc.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE MagicHash #-}
-
 import qualified Prelude as P
 import qualified Data.IORef as D
 import qualified Data.Map as D
@@ -20,7 +19,7 @@ globaldotprimuscoreglobalT_ND# = P.return P.$ Func P.$ \x -> P.return P.$ Func P
   y1 <- y
   let GlobalT_Det s = globaldotprimuscoreglobalT_Det# x1 y1
   P.return P.$ GlobalT_ND (fromHaskell s)
-    
+
 globaldotprimuscorereadGlobalT_Det# (GlobalT_Det s) = do
   store <- D.readIORef globals
   case D.lookup (toForeign s) store of
@@ -28,16 +27,22 @@ globaldotprimuscorereadGlobalT_Det# (GlobalT_Det s) = do
     P.Just v  -> P.return (typed v)
 globaldotprimuscorereadGlobalT_ND# = P.return P.$ Func P.$ \x -> do
   x1 <- x
-  let GlobalT_ND s = x1
-  s1 <- toHaskell s
-  P.return (globaldotprimuscorereadGlobalT_Det# (GlobalT_Det s1))
+  case x1 of
+    GlobalTFlat# (GlobalT_Det s1) -> P.return P.$ globaldotprimuscorereadGlobalT_Det# (GlobalT_Det s1)
+    GlobalT_ND s -> do
+      s1 <- toHaskell s
+      P.return P.$ globaldotprimuscorereadGlobalT_Det# (GlobalT_Det s1)
 
 globaldotprimuscorewriteGlobalT_Det# (GlobalT_Det s) v = fromForeign P.$ do
   store <- D.readIORef globals
   D.writeIORef globals (D.insert (toForeign s) (untyped v) store)
 globaldotprimuscorewriteGlobalT_ND# = P.return P.$ Func P.$ \x -> P.return P.$ Func P.$ \y -> do
   x1 <- x
-  let GlobalT_ND s = x1
-  s1 <- toHaskell s
-  y1 <- y
-  P.return P.$ P.fmap from (globaldotprimuscorewriteGlobalT_Det# (GlobalT_Det s1) (y1))
+  case x1 of
+    GlobalTFlat# (GlobalT_Det s1) -> do
+      y1 <- y
+      P.return P.$ P.fmap from (globaldotprimuscorewriteGlobalT_Det# (GlobalT_Det s1) y1)
+    GlobalT_ND s -> do
+      s1 <- toHaskell s
+      y1 <- y
+      P.return P.$ P.fmap from (globaldotprimuscorewriteGlobalT_Det# (GlobalT_Det s1) y1)
